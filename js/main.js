40e749c91c4919ad2b09bd22fc08cb5c642fa5e4
elloot <54401777+elloot@users.noreply.github.com>
2022-03-22 22:47:03 +0100

    💄 add nicer text animation

--- js/main.js
+++ js/main.js
@@ -10,20 +10,20 @@ class GDPRText {
       (y ??
         window.innerHeight / 2 +
           Math.random() * baseFontSize * (Math.random() > 0.5 ? 1 : -1)) + 'px';
-    this.xVelocity = Math.random() > 0.5 ? -10 : 10;
-    this.yVelocity = Math.random() > 0.5 ? -10 : 10;
+    this.xVelocity = Math.random() > 0.5 ? -5 : 5;
+    this.yVelocity = Math.random() > 0.5 ? -5 : 5;
     this.sinInput = 0;
   }
 
   styleElement() {
     this.element.style.position = 'absolute';
     this.element.style.zIndex = '10000';
-    this.element.style.color = 'red';
     this.element.style.fontSize = `${baseFontSize}px`;
     this.element.innerText = 'GDPR';
     this.element.style.transform = 'translateX(-50%) translateY(-50%)';
     this.element.style.pointerEvents = 'none';
     this.element.style.userSelect = 'none';
+    this.element.classList.add('rainbow_text_animated');
   }
 
   getPos() {
@@ -44,14 +44,36 @@ texts.push(new GDPRText());
 
 document.body.appendChild(texts[0].element);
 
+document.addEventListener('click', (e) => {
+  texts.push(new GDPRText(e.clientX, e.clientY));
+  document.body.appendChild(texts[texts.length - 1].element);
+});
+
+const style = document.createElement('style');
+style.innerHTML = `
+  .rainbow_text_animated {
+    background: linear-gradient(to right, #6666ff, #0099ff , #00ff00, #ff3399, #6666ff);
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+    animation: rainbow_animation 3s infinite;
+    background-size: 400% 100%;
+  }
+
+  @keyframes rainbow_animation {
+    0%,100% {
+        background-position: 0 0;
+    }
+
+    50% {
+        background-position: 100% 0;
+    }
+  }
+`;
+document.querySelector('head').appendChild(style);
+
 function animate() {
   texts.forEach((text) => {
-    r = Math.floor(Math.random() * 256);
-    g = Math.floor(Math.random() * 256);
-    b = Math.floor(Math.random() * 256);
-
-    text.element.style.color = `rgb(${r}, ${g}, ${b})`;
-
     text.setPos(
       text.getPos().x + text.xVelocity,
       text.getPos().y + text.yVelocity
@@ -65,7 +87,7 @@ function animate() {
       text.yVelocity *= -1;
     }
 
-    text.sinInput += 0.03;
+    text.sinInput += 0.02;
 
     text.element.style.fontSize = `${
       (0.4 * Math.sin(text.sinInput) + 0.8) * baseFontSize
@@ -76,8 +98,3 @@ function animate() {
 }
 
 requestAnimationFrame(animate);
-
-document.addEventListener('click', (e) => {
-  texts.push(new GDPRText(e.clientX, e.clientY));
-  document.body.appendChild(texts[texts.length - 1].element);
-});
